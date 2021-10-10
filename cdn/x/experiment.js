2fb28ddab1dd5c12a44c1d7348cea01f9e1d280a
Martin Feineis <mfeineis@users.noreply.github.com>
2021-10-10 15:02:50 +0200

    feat: Simplify experimental component API
    Right now components can be thought of not classes but basically just
    a render function with some lifecycle hooks and convenience wrappers for
    defining attributes and properties.

--- cdn/x/experiment.js
+++ cdn/x/experiment.js
@@ -2,39 +2,112 @@
 
 Core.use(({ log }) => {
 
-    class Base extends HTMLElement {
-        
+    class Base extends HTMLElement {}
+
+    const lens = () => {};
+    const attr = name => ({ $: lens, t: attr, name });
+    const prop = name => ({ $: lens, t: prop, name });
+
+    function define(tag, decl) {
+        const { ...userDecls } = decl;
+
+        const observedAttributes = [];
+        const attrs = new Map();
+        const props = new Map();
+
+        class Component extends Base {
+            static get observedAttributes() {
+                return observedAttributes;
+            }
+            constructor(...args) {
+                log("Component.constructor", ...args);
+                super(...args);
+
+                this.__store__ = new Map();
+                for (const [key, { name }] of attrs.entries()) {
+                    Object.defineProperty(this, key, {
+                        get() {
+                            const value = this.getAttribute(name) ?? null;
+                            log("  Component.attr", key, ".get", value);
+                            this.__store__.set(key, value);
+                            return value;
+                        },
+                        set(value) {
+                            const old = this.__store__.get(key);
+                            if (old === value) {
+                                return;
+                            }
+                            log("  Component.attr", key, ".set", value);
+                            this.__store__.set(key, value);
+                            this.setAttribute(name, value);
+                        }
+                    });
+                }
+                for (const [key, _] of props.entries()) {
+                    Object.defineProperty(this, key, {
+                        get() {
+                            const value = this.__store__.get(key);
+                            log("  Component.prop", key, ".get", value);
+                            return value;
+                        },
+                        set(value) {
+                            log("  Component.prop", key, ".set", value);
+                            this.__store__.set(key, value);
+                        }
+                    });
+                }
+            }
+            connectedCallback() {
+                log("Component.connectedCallback:before");
+                this.componentDidMount();
+                this.render();
+                log("Component.connectedCallback:after", "store", this.__store__);
+            }
+            disconnectedCallback(...args) {
+                log("Component.disconnectedCallback", ...args);
+                this.componentDidUnmount(this);
+            }
+            attributeChangedCallback(name, old, value) {
+                log("Component.attributeChangedCallback", name, old, value);
+                this[name] = value;
+            }
+            adoptedCallback(...args) {
+                log("Component.adoptedCallback", ...args);
+            }
+        }
+
+        for (const [key, value] of Object.entries(userDecls)) {
+            if (value.$ === lens) {
+                if (value.t === attr) {
+                    observedAttributes.push(value.name);
+                    attrs.set(key, value);
+                }
+                if (value.t === prop) {
+                    props.set(key, value);
+                }
+                continue;
+            }
+            Component.prototype[key] = value;
+        }
+
+        customElements.define(tag, Component);
     }
 
-    function define(name, factory) {
-        customElements.define(name, factory(Base));
-    }
+    define("x-experiment", {
+        name: attr("name"),
+        value: prop("value"),
 
-    define("x-experiment", Base => class extends Base {
-        static get observedAttributes() {
-            return ["who"];
-        }
-        constructor(...args) {
-            log("x-experiment.constructor", ...args);
-            super();
-        }
-        connectedCallback(...args) {
-            log("x-experiment.connectedCallback", ...args);
-            this.appendChild(this.ownerDocument.createTextNode(`Experimental ${this._who}`));
-        }
-        disconnectedCallback(...args) {
-            log("x-experiment.disconnectedCallback", ...args);
-        }
-        attributeChangedCallback(...args) {
-            log("x-experiment.attributeChangedCallback", ...args);
-        }
-        adoptedCallback(...args) {
-            log("x-experiment.adoptedCallback", ...args);
-        }
-
-        get _who() {
-            return this.getAttribute("who") ?? "World";
-        }
+        componentDidMount() {
+            log("  x-experiment.componentDidMount", this);
+            this.value = "a value from componentDidMount";
+        },
+        componentDidUnmount() {
+            log("  x-experiment.componentDidUnmount", this);
+        },
+        render() {
+            log("  x-experiment.render", this);
+            this.appendChild(this.ownerDocument.createTextNode(`Experimental ${this.name}`));
+        },
     });
 
 });
